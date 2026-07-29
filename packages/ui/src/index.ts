dbec81b08e29a1d9c17d7a4a31a0d92a3bf4b1ec
Lizzy Liang <lizzy.liang@bigstack.co>
2026-07-29 09:34:15 +0800

    fix: pass Icon to Button for correct story display
    Signed-off-by: Lizzy Liang <lizzy.liang@bigstack.co>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,8 +1,13 @@
+// Components
 export * from './components/CubeButton'
 export * from './components/CubeIcon'
 export * from './components/CubeInput'
 export * from './components/CubeLoadingSpinner'
 export * from './components/CubePasswordInput'
 export * from './components/CubeSkeleton'
+
+// I18n
 export * from './i18n'
+
+// Theme
 export * from './theme'
