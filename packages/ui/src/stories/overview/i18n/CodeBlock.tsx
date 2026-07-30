55fe6b2a79596f1c463e17a023f967febe76074f
Lizzy Liang <lizzy.liang@bigstack.co>
2026-07-30 15:07:57 +0800

    feat: integrate prism-react-renderer for better syntax highlight
    Signed-off-by: Lizzy Liang <lizzy.liang@bigstack.co>

--- packages/ui/src/stories/overview/i18n/CodeBlock.tsx
+++ packages/ui/src/stories/overview/i18n/CodeBlock.tsx
@@ -1,10 +1,32 @@
 import type { PropsWithChildren } from 'react'
+import { Highlight, themes } from 'prism-react-renderer'
+import type { Language } from 'prism-react-renderer'
 
-export const CodeBlock = (props: PropsWithChildren) => {
-  const { children } = props
+type CodeBlockProps = PropsWithChildren & { language?: Language }
+
+export const CodeBlock = (props: CodeBlockProps) => {
+  const { children, language = 'tsx' } = props
+
+  const code = typeof children === 'string' ? children.trim() : ''
+
   return (
-    <pre className="w-full overflow-x-auto rounded-lg bg-dark-50 p-4 text-sm">
-      <code className="font-mono text-dark-800">{children}</code>
-    </pre>
+    <Highlight code={code} language={language} theme={themes.nightOwl}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          className={`${className} w-full overflow-x-auto rounded-lg p-4 text-sm`}
+          style={style}
+        >
+          <code className="font-mono">
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token })} />
+                ))}
+              </div>
+            ))}
+          </code>
+        </pre>
+      )}
+    </Highlight>
   )
 }
